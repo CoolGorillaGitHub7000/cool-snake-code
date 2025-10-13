51a6e969565175dd10af1ffe0f26b6e430e84ae1
Brian McShane <brian.mcshane06@gmail.com>
2025-10-13 13:16:56 -0600

    Added print for name and fav hobby

--- Codingsfile.py
+++ Codingsfile.py
@@ -4,3 +4,8 @@ first_name = "Brian"
 birth_year = 2006
 current_year = 2025
 favorite_hobby = "YouTubing"
+
+# Now here's a print test using my variables.
+
+print(f"Hi and welcome back {first_name}! You seem to have not changed. Your favorite hobby
+      is still {favorite_hobby}")
