2f113e61d2beb5a014340cb703def7e4f8868f15
Brian McShane <brian.mcshane06@gmail.com>
2025-10-13 13:28:46 -0600

    Added age integer imput

--- Codingsfile.py
+++ Codingsfile.py
@@ -4,7 +4,9 @@ first_name = "Brian"
 birth_year = 2006
 current_year = 2025
 favorite_hobby = "YouTubing"
+current_age = int(input("Enter your age: "))
 
 # Now here's a print test using my variables.
 
 print(f"Hi and welcome back {first_name}! You seem to have not changed. Your favorite hobby is still {favorite_hobby}")
+
