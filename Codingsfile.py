ed0481ac567b8a6b89d1ebb3cebbb3ad368edca0
Brian McShane <brian.mcshane06@gmail.com>
2025-10-13 13:18:01 -0600

    Fixed print

--- Codingsfile.py
+++ Codingsfile.py
@@ -7,5 +7,4 @@ favorite_hobby = "YouTubing"
 
 # Now here's a print test using my variables.
 
-print(f"Hi and welcome back {first_name}! You seem to have not changed. Your favorite hobby
-      is still {favorite_hobby}")
+print(f"Hi and welcome back {first_name}! You seem to have not changed. Your favorite hobby is still {favorite_hobby}")
