64c9b1cfe7d3c9685bcd21422ddbd372a7a3fb01
Brian McShane <brian.mcshane06@gmail.com>
2025-10-15 12:55:34 -0600

    Update lab.py

--- lab.py
+++ lab.py
@@ -7,13 +7,13 @@ name = input("Enter your name: ")
 print("Hello " + name + "!" + " Let's calculate your test scores.")
 # 88 was not an integer. It was in quotes.
 scores = [85, 90, 78, 88, 92]
-# Colon was missing from score
+# Colon was missing from scores
 total = 0
 for score in scores:
     total = total + score
 
 average = total / len(scores)
-print("Your average score is:" (average))
+print("Your average score is," (average))
 
 highest = 0
 for s in scores:
