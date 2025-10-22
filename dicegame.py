3b49b5889e90b77ed4d6c5877a30cef3b59d0831
Brian McShane <brian.mcshane06@gmail.com>
2025-10-22 12:02:12 -0600

    Whaterror

--- dicegame.py
+++ dicegame.py
@@ -1,14 +1,16 @@
-import random
-die1 = random.randint(1, 6)
-die2 = random.randint(1, 6)
+# import random
+die1 = 3
+die2 = 6
 total = die1 + die2 
 # When the two dice equal 7 or 11, that's a win. 1+6, 2+5, 3+4, and 5+6
 if (total==7 or total==11):
     print ("You're Winner!")
+# When the dice equal one another, this is considered a doubles win. If they both equal 6, it's a jackpot.
 elif (die1==die2):
     if (die1==6 and die2==6):
         print ("Jackpot! You get no money! Just bragging rights.")
     else:
         print ("Doubles! You're double winner!")
+# If the dice aren't double, or equaled to 7/11, the player loses
 else:
     print ("You're loser!")
