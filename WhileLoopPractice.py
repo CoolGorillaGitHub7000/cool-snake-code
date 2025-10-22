e7e13dfa3d0ac25c1727d89b2b9356e0e5e2ee4b
Brian McShane <brian.mcshane06@gmail.com>
2025-10-22 13:45:08 -0600

    Added content to WhileLopp

---
+++ WhileLoopPractice.py
@@ -0,0 +1,7 @@
+loop_count = 0
+cont = "y"
+input = ("Do you want to continue? Y or N:")
+while (cont=="y" or cont=="Y"):
+    loop_count += 1
+    cont = input ("Do you want to continue? Y or N:")
+    print ("Loop ran") (loop_count) ("times")
