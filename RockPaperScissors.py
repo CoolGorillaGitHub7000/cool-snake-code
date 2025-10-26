e8f2a885b1c5f0ff2fee6157c5da5a63689dc6f3
Brian McShane <brian.mcshane06@gmail.com>
2025-10-26 14:43:13 -0600

    replaced symbols with letters

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -2,7 +2,7 @@
 # Random numbers are required because of the types of inputs and scenarios in this game
 from random import randint
 # This input asks the player to choose one of the three choices using symbols 0, _, and >8
-opponent = input("rock (0), paper (-), or scissors (8<)?")
+opponent = input("rock (r), paper (p), or scissors (s)?")
 # There's an added end because this tells Python to end this line with a space instead of a new line.
 print(opponent, "vs", end=" ")
 
@@ -13,33 +13,33 @@ choice = randint(0,2)
 # The possible scenarios of computer choosing a random number
 
 if(choice == 0):
-    computer = "0"
+    computer = "r"
 
 elif(choice == 1):
-    computer = "-"
+    computer = "p"
 
 elif(choice == 2):
-    computer = "8<"
+    computer = "s"
 
     print(computer)
 
     if(opponent == computer):
         print("It's a draw!")
 
-    elif(opponent == "0" and computer == "8<"):
+    elif(opponent == "r" and computer == "s"):
         print("Congratulations! You win!")
 
-    elif(opponent == "0" and computer == "-"):
+    elif(opponent == "r" and computer == "p"):
         print("Haha! Computer wins! You lose!")
 
-    elif(opponent == "-" and computer == "0"):
+    elif(opponent == "p" and computer == "0"):
         print("Congrats! You win!")
 
-    elif(opponent == "-" and computer == "8<"):
+    elif(opponent == "-" and computer == "s"):
         print("LOL! Computer wins, you lose!")
 
-    elif(opponent == "8<" and computer == "-"):
+    elif(opponent == "s" and computer == "p"):
         print("Wowie Zowie! You win!")
 
-    elif(opponent == "8<" and computer == "0"):
+    elif(opponent == "s" and computer == "r"):
         print("Better luck next time! Computer wins!")
