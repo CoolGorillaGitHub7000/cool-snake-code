42d6941643f2ace6661cd7e12909cbf98dd7330f
Brian McShane <brian.mcshane06@gmail.com>
2025-10-27 13:44:12 -0600

    sefdf

--- another420whaterror.py
+++ another420whaterror.py
@@ -1,2 +1,5 @@
+# This code involves the second number subtracting the first from it to get a range of numbers that begin at the exact initial value and ends at the final value minus one
+# Negative values do work because the range doesn't have a rule to start from zero
+# Decimals in this or any other range values do not work because they infinitely loop.
 for count in range(-4, 4):  
     print(count)
