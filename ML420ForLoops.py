8ef40ade968d2d3138125ac52eec383201be8cff
Brian McShane <brian.mcshane06@gmail.com>
2025-10-27 13:13:27 -0600

    Added another python

--- ML420ForLoops.py
+++ ML420ForLoops.py
@@ -2,5 +2,5 @@
 # 1st: The count variable always begins at the number zero
 # 2nd: The count variable goes up by one. So like 0, 1, 2, 3 and so on.
 # 3rd: The count variable stops at the number before the range number. So if the number is 9, the count stops at 8
-for count in range(9):
+for count in range(1000000):
     print(count)
